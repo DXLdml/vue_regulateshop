7fea967ab2b80746088564176720445b7b11d0d7
dongxiaolin <dongxiaolin@beyondcent.com>
2021-11-30 18:10:21 +0800

    feat（用户）：完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -8,7 +8,17 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 import axios from "axios";
 //配置请求的根路径
-axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/login'
+//axios.defaults.baseURL='http://172.16.20.65:8888/api/private/v1/'
+axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1/'
+//进行拦截
+axios.interceptors.request.use(config => {
+    console.log(config);
+    // 登录授权 请求验证是否有token  需要授权的 API ，必须在请求头中使用 `Authorization` 字段提供 `token` 令牌
+    config.headers.Authorization = window.sessionStorage.getItem('token');
+    // 在最后必须return
+    return config
+})
+//挂载到原型
 Vue.prototype.$http=axios
 Vue.config.productionTip = false
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,30 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from "../components/Login";
 import Home from "../components/Home";
+import Welcome from "../components/Welcome";
+import Users from "../components/user/Users";
 
 Vue.use(VueRouter)
 
 const routes = [
   {path:'/',redirect:'/login'},
   {path:'/login',component:Login},
-  {path:'/home',component:Home}
+  {
+    path:'/home',
+    component:Home,
+    redirect: '/welcome',
+    children:[
+        {
+          path:'/welcome',
+          component:Welcome
+        },
+        {
+          path:'/users',
+          component: Users
+        }
+    ]
+  }
 ]
-
 const router = new VueRouter({
   routes
 })
@@ -26,7 +41,5 @@ if(to.path==='/login') return next()
 const tokenStr =window.sessionStorage.getItem('token')
 if(!tokenStr) return next('/login')
   next()
-
-
 })
 export default router
